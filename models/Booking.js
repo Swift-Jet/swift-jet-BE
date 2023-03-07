fbd76ea92f68640fa793b31352cbe01b9fe01fe7
Babajide Ojo <babajide.ojo@konga.com>
2023-03-07 11:50:05 +0100

    booking algorithm  updatedc

--- models/Booking.js
+++ models/Booking.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
-
 const BookingSchema = new mongoose.Schema({
- user:{
-  type: Object
- },
- phone_number: {
-  type: String
- },
- booking_details:{
-  type: Object
- },
- additional_quote:{
-  type: Array
- }
-
+  booking_number: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ["New", "Processing", "Confirmed"],
+    default: "New",
+  },
+  user: {
+    type: Object,
+  },
+  booking_details: {
+    type: Object,
+  },
+  additional_quote: {
+    type: Array,
+  },
 });
 
 module.exports = mongoose.model("Booking", BookingSchema);
